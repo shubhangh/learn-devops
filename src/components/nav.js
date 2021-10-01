bea81d57b580b56c0bf8b6ed6a05e9ab469d942e
shu <34476035+shubhangh@users.noreply.github.com>
2021-09-30 23:01:04 -0400

    Basic Navigation menu. (#2)

---
+++ src/components/nav.js
@@ -0,0 +1,13 @@
+function NavigationMenu() {
+  return (
+    <div className="navigationMenu">
+      <ul>
+        <li>Home</li>
+        <li>Login</li>
+        <li>Logout</li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavigationMenu;
